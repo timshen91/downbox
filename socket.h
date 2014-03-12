ed1cd55cf79c787faf316229a3df7b66de5717bc
tim <timshen91@gmail.com>
2014-03-11 20:32:15 -0400

    Change tabs to spaces.

--- socket.h
+++ socket.h
@@ -13,72 +13,72 @@ class TCPSocket {
 
 public:
     bool init(const char* dest_addr, uint16_t port) {
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-	    perror("socket");
-	    return false;
-	}
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-	if (inet_aton(dest_addr, &addr.sin_addr) == 0) {
-	    perror("inet_aton");
-	    return false;
-	}
-	if (connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-	    perror("connect");
-	    return false;
-	}
-	return true;
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            perror("socket");
+            return false;
+        }
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        if (inet_aton(dest_addr, &addr.sin_addr) == 0) {
+            perror("inet_aton");
+            return false;
+        }
+        if (connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+            perror("connect");
+            return false;
+        }
+        return true;
     }
 
     void close() {
-	::close(sockfd);
+        ::close(sockfd);
     }
 
     bool read(void* buff, ssize_t len) {
-	while (len > 0) {
-	    ssize_t count = len;
-	    if (count > SSIZE_MAX) {
-		count = SSIZE_MAX;
-	    }
-	    auto n = ::read(sockfd, buff, count);
-	    if (n < 0) {
-		perror("read");
-		return false;
-	    }
-	    if (n == 0) {
-		return false;
-	    }
-	    len -= n;
-	    buff = (void*)((uintptr_t)buff + count);
-	}
-	return true;
+        while (len > 0) {
+            ssize_t count = len;
+            if (count > SSIZE_MAX) {
+                count = SSIZE_MAX;
+            }
+            auto n = ::read(sockfd, buff, count);
+            if (n < 0) {
+                perror("read");
+                return false;
+            }
+            if (n == 0) {
+                return false;
+            }
+            len -= n;
+            buff = (void*)((uintptr_t)buff + count);
+        }
+        return true;
     }
 
     template<typename T>
-    bool read(T* obj) {
-	return read(obj, sizeof(T));
-    }
+      bool read(T* obj) {
+          return read(obj, sizeof(T));
+      }
 
     bool write(const void* buff, int len) {
-	while (len > 0) {
-	    int n = ::write(sockfd, buff, len);
-	    if (n < 0) {
-		perror("write");
-		return false;
-	    }
-	    if (n == 0) {
-		return false;
-	    }
-	    len -= n;
-	    buff = (void*)((uintptr_t)buff + n);
-	}
-	return true;
+        while (len > 0) {
+            int n = ::write(sockfd, buff, len);
+            if (n < 0) {
+                perror("write");
+                return false;
+            }
+            if (n == 0) {
+                return false;
+            }
+            len -= n;
+            buff = (void*)((uintptr_t)buff + n);
+        }
+        return true;
     }
 
     template<typename T>
-    bool write(const T& obj) {
-	return write(&obj, sizeof(T));
-    }
+      bool write(const T& obj) {
+          return write(&obj, sizeof(T));
+      }
 };
 
 class TCPServer {
@@ -87,38 +87,38 @@ class TCPServer {
 
 public:
     bool init(const char* listen_addr, uint16_t port) {
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-	    perror("socket");
-	    return false;
-	}
-	int yes = 1;
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0) {
-	    perror("setsockopt");
-	    return false;
-	}
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-	if (inet_aton(listen_addr, &addr.sin_addr) == 0) {
-	    perror("inet_aton");
-	    return false;
-	}
-	if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-	    perror("bind");
-	    return false;
-	}
-	if (listen(sockfd, 10) < 0) {
-	    perror("listen");
-	    return false;
-	}
-	return true;
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            perror("socket");
+            return false;
+        }
+        int yes = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0) {
+            perror("setsockopt");
+            return false;
+        }
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        if (inet_aton(listen_addr, &addr.sin_addr) == 0) {
+            perror("inet_aton");
+            return false;
+        }
+        if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+            perror("bind");
+            return false;
+        }
+        if (listen(sockfd, 10) < 0) {
+            perror("listen");
+            return false;
+        }
+        return true;
     }
 
     void close() {
-	::close(sockfd);
+        ::close(sockfd);
     }
 
     bool accept(TCPSocket* cli) {
-	socklen_t len = sizeof(cli->addr);
-	return (cli->sockfd = ::accept(sockfd, (struct sockaddr*)&cli->addr, &len)) >= 0;
+        socklen_t len = sizeof(cli->addr);
+        return (cli->sockfd = ::accept(sockfd, (struct sockaddr*)&cli->addr, &len)) >= 0;
     }
 };
