b28a47a90bf39622b944789b31d92dd9a82a5b59
tim <timshen91@gmail.com>
2014-03-22 19:39:44 -0400

    Revised endian.h

--- endian.h
+++ endian.h
@@ -9,80 +9,48 @@ namespace {
 template<int>
 struct uint;
 
-template<>
-struct uint<1> {
-    typedef uint8_t type;
-};
+#define UINT_GEN(n, T) \
+template<>\
+struct uint<n> {\
+    typedef T type;\
+}
 
-template<>
-struct uint<2> {
-    typedef uint16_t type;
-};
+UINT_GEN(1, uint8_t);
+UINT_GEN(2, uint16_t);
+UINT_GEN(4, uint32_t);
+UINT_GEN(8, uint64_t);
 
-template<>
-struct uint<4> {
-    typedef uint32_t type;
-};
-
-template<>
-struct uint<8> {
-    typedef uint64_t type;
-};
+#undef UINT_GEN
 
 template<int size>
 using uint_t = typename uint<size>::type;
 
-}
-
 template<int size>
 uint_t<size> letoh_impl(uint_t<size> a);
 
-template<>
-uint_t<1> letoh_impl<1>(uint_t<1> a) {
-    return a;
+template<int size>
+uint_t<size> htole_impl(uint_t<size> a);
+
+#define ENDIAN_IMPL(n, f, g) \
+template<>\
+uint_t<n> f<n>(uint_t<n> a) {\
+    return g(a);\
 }
 
-template<>
-uint_t<2> letoh_impl<2>(uint_t<2> a) {
-    return le16toh(a);
-}
+ENDIAN_IMPL(1, letoh_impl, );
+ENDIAN_IMPL(2, letoh_impl, le16toh);
+ENDIAN_IMPL(4, letoh_impl, le32toh);
+ENDIAN_IMPL(8, letoh_impl, le64toh);
+ENDIAN_IMPL(1, htole_impl, );
+ENDIAN_IMPL(2, htole_impl, htole16);
+ENDIAN_IMPL(4, htole_impl, htole32);
+ENDIAN_IMPL(8, htole_impl, htole64);
 
-template<>
-uint_t<4> letoh_impl<4>(uint_t<4> a) {
-    return le32toh(a);
-}
-
-template<>
-uint_t<8> letoh_impl<8>(uint_t<8> a) {
-    return le64toh(a);
 }
 
 template<typename T>
 T letoh(T a) {
     return letoh_impl<sizeof(T)>(a);
-}
-
-template<int size>
-uint_t<size> htole_impl(uint_t<size> a);
-
-template<>
-uint_t<1> htole_impl<1>(uint_t<1> a) {
-    return a;
-}
-
-template<>
-uint_t<2> htole_impl<2>(uint_t<2> a) {
-    return htole16(a);
-}
-
-template<>
-uint_t<4> htole_impl<4>(uint_t<4> a) {
-    return htole32(a);
-}
-
-template<>
-uint_t<8> htole_impl<8>(uint_t<8> a) {
-    return htole64(a);
 }
 
 template<typename T>
