ce1bce55d0e438b97f4adaccd9123dddbd860f24
noeyfan <youfan.noey@gmail.com>
2014-04-07 15:02:39 -0400

    delete

--- WinClient/WinClient/Source.cpp
+++ WinClient/WinClient/Source.cpp
@@ -1,3 +1,7 @@
+//Function for now
+//1.push local file to cloud
+//2.delete from cloud
+
 #include <stdio.h>
 #include <tchar.h>
 #include <iostream>
@@ -47,11 +51,11 @@ int monitor(){
 		printf("File already exists.\n");
 	}
 	HANDLE dirHandle = CreateFile(dir, FILE_LIST_DIRECTORY,
-		FILE_SHARE_WRITE | FILE_SHARE_READ | FILE_SHARE_DELETE,
-		NULL,
-		OPEN_EXISTING,
-		FILE_FLAG_BACKUP_SEMANTICS,
-		NULL);
+			FILE_SHARE_WRITE | FILE_SHARE_READ | FILE_SHARE_DELETE,
+			NULL,
+			OPEN_EXISTING,
+			FILE_FLAG_BACKUP_SEMANTICS,
+			NULL);
 
 	DWORD cbBytes;
 
@@ -59,54 +63,60 @@ int monitor(){
 	FILE_NOTIFY_INFORMATION *pnotify = (FILE_NOTIFY_INFORMATION*)notify;
 	char Namestr[1024];
 
-	ReqCreateFile req;
 	while (TRUE){
 		if (ReadDirectoryChangesW(dirHandle, &notify, sizeof(notify), TRUE,
-			FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_DIR_NAME
-			| FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_LAST_WRITE,
-			&cbBytes, NULL, NULL)) {
+					FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_DIR_NAME
+					| FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_LAST_WRITE,
+					&cbBytes, NULL, NULL)) {
 			switch (pnotify->Action) {
-			case FILE_ACTION_ADDED: {
-										//show file_change info in console
-										wchar_to_char(pnotify->FileName, Namestr, pnotify->FileNameLength);
-										cout << "added   " << Namestr << endl;
-										//send file
-										conn << (uint8_t)CREATE_FILE;
-										req.get<0>().assign(Namestr);
-										ifstream fin(Namestr);
-										if (fin.fail()) {
-											cout << "init file stream fail";
-										}
-										if (!fin.bad()) {
-											cout << fin.rdbuf();
-											fin.close();
-										}
-										fin.seekg(0, ifstream::end);
-										req.get<1>().resize(fin.tellg());
-										fin.seekg(0,ifstream::beg);
-										fin.read(req.get<1>().data(), req.get<1>().size());
-										fin.close();
-										conn << req;
-										break;
-			}
-			case FILE_ACTION_MODIFIED:
-				wchar_to_char(pnotify->FileName, Namestr, pnotify->FileNameLength);
-				cout << "modified  " << Namestr;
-			case FILE_ACTION_REMOVED:
-				wchar_to_char(pnotify->FileName, Namestr, pnotify->FileNameLength);
-				cout << "removed  " << Namestr << endl;
-				break;
-			case FILE_ACTION_RENAMED_OLD_NAME:
-				wchar_to_char(pnotify->FileName, Namestr, pnotify->FileNameLength);
-				cout << "renamed  " << Namestr << endl;
-				break;
-			case FILE_ACTION_RENAMED_NEW_NAME:
-				//FIXME new file name will not show
-				wchar_to_char(pnotify->FileName, Namestr, pnotify->FileNameLength);
-				cout << "to name  " << Namestr << endl;
-				break;
-			default:
-				printf("unkonw action\n");
+				case FILE_ACTION_ADDED: {
+																	//show file_change info in console
+																	wchar_to_char(pnotify->FileName, Namestr, pnotify->FileNameLength);
+																	cout << "added   " << Namestr << endl;
+																	//send file
+																	ReqCreateFile req;
+																	conn << (uint8_t)CREATE_FILE;
+																	req.get<0>().assign(Namestr);
+																	ifstream fin(Namestr);
+																	if (fin.fail()) {
+																		cout << "init file stream fail";
+																	}
+																	if (!fin.bad()) {
+																		cout << fin.rdbuf();
+																		fin.close();
+																	}
+																	fin.seekg(0, ifstream::end);
+																	req.get<1>().resize(fin.tellg());
+																	fin.seekg(0,ifstream::beg);
+																	fin.read(req.get<1>().data(), req.get<1>().size());
+																	fin.close();
+																	conn << req;
+																	// TODO, list file every time it makes changes
+																	break;
+																}
+				case FILE_ACTION_MODIFIED:
+																wchar_to_char(pnotify->FileName, Namestr, pnotify->FileNameLength);
+																cout << "modified  " << Namestr;
+				case FILE_ACTION_REMOVED:
+																wchar_to_char(pnotify->FileName, Namestr, pnotify->FileNameLength);
+																cout << "removed  " << Namestr << endl;
+																//delete file
+																conn << (uint8_t)DELETE_;
+																ReqDelete req;
+																req.assign(Namestr);
+																conn << req;
+																break;
+				case FILE_ACTION_RENAMED_OLD_NAME:
+																wchar_to_char(pnotify->FileName, Namestr, pnotify->FileNameLength);
+																cout << "renamed  " << Namestr << endl;
+																break;
+				case FILE_ACTION_RENAMED_NEW_NAME:
+																//FIXME new file name will not show
+																wchar_to_char(pnotify->FileName, Namestr, pnotify->FileNameLength);
+																cout << "to name  " << Namestr << endl;
+																break;
+				default:
+																printf("unkonw action\n");
 			}
 		}
 	}
